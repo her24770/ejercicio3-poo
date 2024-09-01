73839ba0b2f4fb9796bc34d1b018f9e1761fd612
her24770 <her24770@uvg.edu.gt>
2024-09-01 00:26:20 -0600

    Controlador de libros terminado con uso de CSV

--- Main.java
+++ Main.java
@@ -1,11 +1,35 @@
+import java.util.List;
 import java.util.Scanner;
+import java.io.File;
+
+import controllers.LibroController;
+import modells.Libro;
 
 public class Main {
     public static void main(String[] args) {
+        //Controllers
+        LibroController libroC = new LibroController();
+        //Array List
+        List<Libro> libros=libroC.listLibros();
+        List<String> listaGenerosLibros = List.of("Fantasia", "Novela", "ficcion", "Poesia","Geografia", "Didatico","Infantil","Biografias");
         Scanner sc = new Scanner(System.in);
+
         System.out.println("------Bienvenido----");
         
         Boolean repeatMenu=true;
+
+        String nombreArchivo = "libros.csv";
+         
+        List<String> archivosCSV= List.of("libros.csv");
+        for(String archivoCSV:archivosCSV){
+            File archivo = new File(archivoCSV);
+            Boolean archivoExiste = archivo.exists() && archivo.isFile();
+            if (!archivoExiste){
+                System.out.println("El archivo '" + nombreArchivo + "' no existe!!");
+                repeatMenu=false;
+            }
+        }
+
         while (repeatMenu) {
             System.out.print("\n------MENU---"+
                                 "\n1. Agregar nuevo Libro"+
@@ -21,11 +45,49 @@ public class Main {
             String otpionMenu = sc.nextLine();
             switch (otpionMenu) {
                 case "1":
+                    Libro newLibro = new Libro();
+                    System.out.println("\n---- NUEVO LIBRO ----");
+                    System.out.print("Ingrese el isbn del libro : ");
+                    String isbnNewLibro = sc.nextLine();
+                    Boolean existLibro = true;
+                    while(existLibro){
+                        existLibro=false;
+                        for(Libro libro:libros){
+                            if(libro.getIsbn().equals(isbnNewLibro)){
+                                existLibro=true;
+                            }
+                        }
+                        if (existLibro) {
+                            System.out.print("Ya existe este ISBN elija otro :");
+                            isbnNewLibro=sc.nextLine();
+                        }else{
+                            newLibro.setIsbn(isbnNewLibro);
+                        }
+                    }
                     
+                    System.out.print("Ingrese el titulo : ");
+                    newLibro.setTitulo(sc.nextLine());
+                    System.out.print("Ingrese el autor : ");
+                    newLibro.setAutor(sc.nextLine());
+                    System.out.print("Ingrese la fecha de publicacion : ");
+                    newLibro.setFechaPublicacion(sc.nextLine());
+                    System.out.println("Seleccione el genero : ");
+                    for(int i =1;i<listaGenerosLibros.size();i++){
+                        System.out.println("   "+i+". "+listaGenerosLibros.get(i-1));
+                    }
+                    System.out.print("   Eleccion : ");
+                    newLibro.setGenero(listaGenerosLibros.get(Integer.parseInt(sc.nextLine())-1));
+                    System.out.print("Ingrese la sucursal del libro : ");
+                    //listar las sucursales
+                    newLibro.setIdSucursal(Integer.parseInt(sc.nextLine()));
+                    System.out.println("Ingrese cuantas copias hay disponibles : ");
+                    newLibro.setDisponibles(Integer.parseInt(sc.nextLine()));
+                    libroC.addLibro(newLibro);
+                    libros=libroC.listLibros();
                     break;
 
                 case "2":
-                    
+
                     break;
 
                 case "3":
--- modells/Libro.java
+++ modells/Libro.java
@@ -4,6 +4,75 @@ package modells;
  * Libro
  */
 public class Libro {
+    private String isbn;
+    private String titulo; 
+    private String autor;
+    private String fechaPublicacion;
+    private String genero;
+    private int idSucursal;
+    private int disponibles;
 
+    public Libro(String isbn, String titulo, String autor, String fechaPublicacion, String genero, int idSucursal,
+            int disponibles) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.fechaPublicacion = fechaPublicacion;
+        this.genero = genero;
+        this.idSucursal = idSucursal;
+        this.disponibles = disponibles;
+    }
+
+    public Libro() {
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+    public void setFechaPublicacion(String fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public int getIdSucursal() {
+        return idSucursal;
+    }
+    public void setIdSucursal(int idSucursal) {
+        this.idSucursal = idSucursal;
+    }
+    public int getDisponibles() {
+        return disponibles;
+    }
+    public void setDisponibles(int disponibles) {
+        this.disponibles = disponibles;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro [isbn=" + isbn + ", titulo=" + titulo + ", autor=" + autor + ", fechaPublicacion="
+                + fechaPublicacion + ", genero=" + genero + ", idSucursal=" + idSucursal + ", disponibles="
+                + disponibles + "]";
+    }
     
 }
