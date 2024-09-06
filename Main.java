64994b03beac8f4beaab9c313b0bcc50ceca62f9
Juan Daniel Ordoñez <ord24979@uvg.edu.gt>
2024-09-05 22:13:48 -0600

    Correccion en import

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 import controllers.LibroController;
 import controllers.MiembroController;
-import modells.Libro;
+import modells.*;
 
 public class Main {
     public static void main(String[] args) {
