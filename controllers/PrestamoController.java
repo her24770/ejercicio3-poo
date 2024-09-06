b9498804639adc952e04018bbbaab9175d2712cb
Gabrexcitado <gabrex08@gmail.com>
2024-09-05 21:41:22 -0600

    Javadoc clase prestamo e inicio de trabajo en prestamoController

--- controllers/PrestamoController.java
+++ controllers/PrestamoController.java
@@ -1,5 +1,67 @@
 package controllers;
 
+import modells.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrestamoController {
+    public List<Prestamo> listPrestamos(){
+        List<Libro> libros = new ArrayList<>();
+
+       try (BufferedReader br = new BufferedReader(new FileReader("libros.csv"))) {
+           String linea;
+           boolean esPrimeraLinea = true;
+
+           while ((linea = br.readLine()) != null) {
+               // Saltar la primera línea que contiene encabezados
+               if (esPrimeraLinea) {
+                   esPrimeraLinea = false;
+                   continue;
+               }
+               // Dividir la linea por atibutos en List
+               String[] valores = linea.split(",");
+               // Definir el valor de cada atributo
+               String isbn = valores[0].trim();
+               String titulo = valores[1].trim();
+               String autor = valores[2].trim();
+               String fechaPublicacion = valores[3].trim();
+               String genero = valores[4].trim();
+               int idSucursal = Integer.parseInt(valores[5].trim());
+               int disponibles = Integer.parseInt(valores[6].trim());
+               //guardar en csv
+               Libro libro = new Libro(isbn, titulo, autor, fechaPublicacion, genero, idSucursal, disponibles);
+               libros.add(libro);
+           }
+       } catch (IOException e) {
+           System.err.println("Error al leer el archivo CSV: " + e.getMessage());
+       }
+       return libros;
+   }
+
+   
+   public void addLibro(Libro newLibro){
+       try (BufferedWriter bw = new BufferedWriter(new FileWriter("libros.csv", true))) {
+           // Crear cadnea String
+           String newLine = newLibro.getIsbn() + "," + 
+                            newLibro.getTitulo() + "," + 
+                            newLibro.getAutor() + "," + 
+                            newLibro.getFechaPublicacion() + "," + 
+                            newLibro.getGenero() + "," + 
+                            newLibro.getIdSucursal() + "," + 
+                            newLibro.getDisponibles();
+           
+           // Escribir la nueva línea al archivo CSV con linea de salot
+           bw.write(newLine);
+           bw.newLine();  
+       } catch (IOException e) {
+           System.err.println("Error al escribir en el archivo CSV: " + e.getMessage());
+       }
+   }
     
 }
